fa0a765aa5013942a867ddbd17fec88e818e90fd
Ali Baydur <67105447+yagizgil@users.noreply.github.com>
2026-03-03 01:37:21 +0300

    group snapshot data by stage

--- src/utils/devtools/inspector/mod.rs
+++ src/utils/devtools/inspector/mod.rs
@@ -1,6 +1,7 @@
-use serde::Serialize;
-use crate::lexer::token::Token;
 use crate::ast::stmt::Stmt;
+use crate::lexer::token::Token;
+use serde::Serialize;
+use std::collections::BTreeMap;
 
 #[derive(Serialize, Clone, Debug)]
 pub struct Snapshot {
@@ -20,7 +21,11 @@ use std::sync::{Mutex, OnceLock};
 static HISTORY: OnceLock<Mutex<InspectorHistory>> = OnceLock::new();
 
 pub fn get_history() -> &'static Mutex<InspectorHistory> {
-    HISTORY.get_or_init(|| Mutex::new(InspectorHistory { snapshots: Vec::new() }))
+    HISTORY.get_or_init(|| {
+        Mutex::new(InspectorHistory {
+            snapshots: Vec::new(),
+        })
+    })
 }
 
 pub fn record(stage: &str, message: &str, tokens: &[Token], ast: &[Stmt]) {
@@ -33,18 +38,29 @@ pub fn record(stage: &str, message: &str, tokens: &[Token], ast: &[Stmt]) {
     });
 }
 
-
 pub fn dump_to_file(filename: &str) {
     if let Ok(history) = get_history().lock() {
-        match serde_json::to_string_pretty(&*history) {
+        let mut grouped_history = BTreeMap::new();
+
+        for snapshot in history.snapshots.iter() {
+            grouped_history
+                .entry(snapshot.stage.clone())
+                .or_insert_with(Vec::new)
+                .push(snapshot.clone());
+        }
+
+        match serde_json::to_string_pretty(&grouped_history) {
             Ok(json) => {
                 if std::fs::write(filename, json).is_ok() {
                     println!(
-                        "\n\x1b[32m[VEX]\x1b[0m Inspection data successfully exported to: \x1b[34m'{}'\x1b[0m", 
+                        "\n\x1b[32m[VEX]\x1b[0m Inspection data successfully exported to: \x1b[34m'{}'\x1b[0m",
                         filename
                     );
                 } else {
-                    eprintln!("\n\x1b[31m[ERROR]\x1b[0m Failed to write inspection data to '{}'", filename);
+                    eprintln!(
+                        "\n\x1b[31m[ERROR]\x1b[0m Failed to write inspection data to '{}'",
+                        filename
+                    );
                 }
             }
             Err(e) => eprintln!("\n\x1b[31m[ERROR]\x1b[0m Serialization failed: {}", e),
